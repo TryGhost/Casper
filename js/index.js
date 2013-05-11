162b362e4bd4aa32564ab1de860ceacba54b7096
Hannah Wolfe <erisds@gmail.com>
2013-05-11 11:16:11 +0100

    Casper - initial commit to separate GitHub repo

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Placeholder JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function () {
+        // code goes here
+    });
+}(jQuery));
