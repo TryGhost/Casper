701746c10205d80b71d3f17418b48e2293096c50
John O'Nolan <john@onolan.org>
2013-08-21 16:44:09 +0200

    Move home header image to inline img / refactor headings

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,14 +8,6 @@
 
     $(document).ready(function(){
 
-        sizeContent();
-        $(window).resize(sizeContent);
-
-        function sizeContent() {
-            var newHeight = $(window).height() - 30;
-            $("#sitehead").css("height", newHeight);
-        }
-
     });
 
 }(jQuery));
