4616c5bd1dd0899741021d451108afa39130b611
John O'Nolan <john@tryghost.org>
2013-10-31 18:35:57 +0100

    Responsive videos by default u guyz

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,10 +8,7 @@
 
     $(document).ready(function(){
 
-        // On the home page, move the blog icon inside the header 
-        // for better relative/absolute positioning.
-
-        //$("#blog-logo").prependTo("#site-head-content");
+        $(".post-content").fitVids();
 
     });
 
