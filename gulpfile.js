1c06a9787d64af177d49a7d1a88c73c2b558c554
William H McMahan Jr <will@mcmahan.me>
2014-04-20 00:40:19 -0400

    stage 1 of gulp implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,48 @@
-// var gulp = require('gulp'),
-// 	watch = require('gulp-watch'),
-// 	sass = require('gulp-sass'),
-// 	cache = require('gulp-cache');
-
-// 	gulp.task('default', ['watch']);
-// 	gulp.task('watch', function() {
-// 		gulp.watch('assets/sass/**', ['sass']);
-// 	});
-
-	// gulp.task('sass', function() {
-	// 	gulp.src('assets/sass/app.scss')
-	// 	.pipe(sass({
-	// 		noCache: false,
-	// 		style: 'expanded',
-	// 		lineNumbers: true,
-	// 		sourcemap: false,
-	// 		quiet: true,
-	// 		includePath: 'assets/sass/'
-	// 	}))
-	// 	.pipe(gulp.dest('assets/css'));
-	// });
-
-
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+
+var sass = require('gulp-sass'),
+	autoprefix = require('gulp-autoprefixer'),
+	jshint = require('gulp-jshint'),
+	imagemin = require('gulp-imagemin'),
+	notify = require('gulp-notify');
 
 
-
-
-
-
-
+// Stylesheets
 
 gulp.task('sass', function () {
 	gulp.src('./assets/sass/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('./assets/css'));
+		.pipe(autoprefix('last 2 versions'))
+		.pipe(gulp.dest('./assets/css'))
+		.pipe(notify({ message: 'Stylesheet build task complete' }));
+});
+
+// Javascript
+gulp.task('jshint', function () {
+	gulp.src('./assets/js/*')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Images
+
+gulp.task('imagemin', function () {
+	gulp.src('./assets/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest(''))
+		.pipe(notify({ message: 'Image minification task complete' }));
+});
+
+
+gulp.task('default', function() {
+
+	// Watch sass files
+	gulp.watch('./assets/sass/**/*.scss', ['sass']);
+
+	// Watch js files
+	gulp.watch('./assets/scripts/**/*.js', ['scripts']);
+
+	// Watch image files
+	gulp.watch('./assets/images/**/*', ['imagemin']);
+
 });
