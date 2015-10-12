5a31289158dbf3e8deb7e8c84ac04faf9cc962c6
David Lecina <dlecina13@gmail.com>
2015-10-12 19:58:54 +0200

    Navigation TryGhost/Casper@46dc55769e5f11bddde77ae131f0cb24f82fd9ea

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,6 +59,11 @@
 
         $(".scroll-down").arctic_scroll();
 
+        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
+            e.preventDefault();
+            $("body").toggleClass("nav-opened nav-closed");
+        });
+
     });
 
     // smartresize
