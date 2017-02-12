f019469201eebdbb85880f871101da65599557e0
David Lecina <dlecina13@gmail.com>
2017-02-12 18:47:42 +0100

    Change search result count text

--- assets/js/jquery.ghostHunter-init.js
+++ assets/js/jquery.ghostHunter-init.js
@@ -1,6 +1,6 @@
 $("#search-field").ghostHunter({
     results : "#search-results",
     onKeyUp : true,
-    info_template   : "<p>Found {{amount}} posts</p>",
+    info_template   : "<p>Results: {{amount}}</p>",
     result_template : "<a href='{{link}}'><p><h2>{{title}}</h2><h4>{{pubDate}}</h4>{{description}}</p></a>"
 });
