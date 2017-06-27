d3a9af0666aa987be19ce44367d2cc8ea18dac53
汪磊 <ice@micua.com>
2017-06-27 19:03:51 +0800

    Remove infinitescroll in single page (#319)
    * Remove infinitescroll in single page
    
    * Update infinitescroll.js

--- assets/js/infinitescroll.js
+++ assets/js/infinitescroll.js
@@ -1,18 +1,29 @@
 // Code snippet inspired by https://github.com/douglasrodrigues5/ghost-blog-infinite-scroll
-$().ready(function () {
-    var page = 2,
-        blogUrl = window.location,
+$(function ($) {
+    var currentPage = 1,
+        pathname = window.location.pathname,
+        $window = $(window),
+        $document = $(document),
         $result = $('.post-feed');
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-            if (page <= maxPages) {
-                $.get((blogUrl + '/page/' + page),
-                function (content) {
-                    $result.append($(content).find('.post').hide().fadeIn(100));
-                    page = page + 1;
-                });
-            }
+    function handleScroll () {
+        // return if not scroll to the bottom
+        if ($window.scrollTop() + $window.height() !== $document.height()) {
+            return;
         }
-    });
+
+        if (currentPage >= maxPages) {
+            return $window.off('scroll', handleScroll);
+        }
+
+        // next page
+        currentPage++;
+
+        // Load more
+        $.get((pathname + 'page/' + currentPage + '/'), function (content) {
+            $result.append($(content).find('.post').hide().fadeIn(100));
+        });
+    }
+
+    $window.on('scroll', handleScroll).trigger('scroll');
 });
