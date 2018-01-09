e23073918daadd4295802cbf334be32696f78737
Chris Manson <chris@stonecircle.io>
2018-01-09 18:10:47 +0000

    getting this to work

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var customProperties = require('postcss-custom-properties');
 var postcssImport = require('postcss-import');
 const debug = require('debug')('ember-casper-template:index');
 
-const StaticSiteJson = require('broccoli-static-site-json');
-const MergeTrees = require('broccoli-merge-trees');
-const Funnel = require('broccoli-funnel');
-
-const attributes = ['uuid', 'title', 'slug', 'image', 'featured', 'page', 'status', 'language', 'meta_title', 'meta_description', 'date', 'tags'];
-const references = ['author']
+// const StaticSiteJson = require('broccoli-static-site-json');
+// const MergeTrees = require('broccoli-merge-trees');
+// const Funnel = require('broccoli-funnel');
+//
+// const attributes = ['uuid', 'title', 'slug', 'image', 'featured', 'page', 'status', 'language', 'meta_title', 'meta_description', 'date', 'tags'];
+// const references = ['author']
 
 module.exports = {
   name: 'ember-casper-template',
@@ -32,34 +32,34 @@ module.exports = {
     }
   },
 
-  treeForApp() {
-    let tree = this._super.treeForApp.apply(this, arguments);
-
-    debug('what about this', tree.inputPaths)
-
-    const jsonTrees = ['content', 'page'].map((contentFolder) => {
-      return new StaticSiteJson(`${__dirname}/${contentFolder}`, {
-        attributes,
-        references,
-        contentFolder,
-        collections: [{
-          src: contentFolder,
-          output: `${contentFolder}.json`,
-        }],
-      });
-    });
-
-    const authorTree = new StaticSiteJson(`${__dirname}/author`, {
-      contentFolder: 'author',
-      attributes: ['name', 'image', 'cover', 'bio', 'website', 'location'],
-      collections: [{
-        src: 'author',
-        output: 'author.json',
-      }]
-    });
-
-    return new MergeTrees([tree, ...jsonTrees, authorTree]);
-  },
+  // treeForAddon() {
+  //   let tree = this._super.treeForApp.apply(this, arguments);
+  //
+  //   debug('what about this', tree.inputPaths)
+  //
+  //   const jsonTrees = ['content', 'page'].map((contentFolder) => {
+  //     return new StaticSiteJson(contentFolder, {
+  //       attributes,
+  //       references,
+  //       contentFolder,
+  //       collections: [{
+  //         src: contentFolder,
+  //         output: `${contentFolder}.json`,
+  //       }],
+  //     });
+  //   });
+  //
+  //   const authorTree = new StaticSiteJson(`author`, {
+  //     contentFolder: 'author',
+  //     attributes: ['name', 'image', 'cover', 'bio', 'website', 'location'],
+  //     collections: [{
+  //       src: 'author',
+  //       output: 'author.json',
+  //     }]
+  //   });
+  //
+  //   return new MergeTrees([tree, ...jsonTrees, authorTree]);
+  // },
 
 
   included(app) {
