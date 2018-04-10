1c635edb7f85def0f68f7ae39f37f21ca1593ebb
Nick Felicelli <nicholas.felicelli@cchmc.org>
2018-04-10 14:19:15 -0400

    Fixing author cover image display

--- app/controllers/author.js
+++ app/controllers/author.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 import { get, computed } from '@ember/object';
 
 export default Controller.extend({
-  coverImageStyle: computed('cover_image', function() {
-    return `background-image: url(${get(this, 'cover_image')})`
+  coverImageStyle: computed('model.cover', function() {
+    return `background-image: url(${get(this, 'model.cover')})`
   })
 })
