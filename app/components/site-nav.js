0d35138b64a7e4675ae7737a7ac4213249aa9572
Chris Manson <chris@stonecircle.io>
2018-04-12 00:15:12 +0100

    fixing rss feed

--- app/components/site-nav.js
+++ app/components/site-nav.js
@@ -1,11 +1,14 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-
-// import { get, computed } from '@ember/object';
+import { get, computed } from '@ember/object';
 
 export default Component.extend({
   blog: service(),
   router: service(),
   url: service(),
   tagName: '',
+
+  rssFeed: computed('blog.host', function() {
+    return `https://feedly.com/i/subscription/feed${encodeURIComponent('/' + get(this, 'blog.host') + '/rss.xml')}`;
+  })
 })
