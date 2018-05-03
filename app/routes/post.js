387a4c4f07a221802a7ff22de22bf21d236d4451
Chris Manson <chris@stonecircle.io>
2018-05-03 11:11:10 +0100

    moving over to the new service based ember-meta

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,28 +1,10 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
-import PostMetaMixin from 'ember-meta/mixins/post-meta';
 import { get } from '@ember/object';
-import { getExcerpt } from '../helpers/excerpt';
 
-export default Route.extend(PostMetaMixin, {
+export default Route.extend({
   classNames: ["post-template"],
 
-  init() {
-    this._super(...arguments);
-    this.attributeReferences = {
-      author: 'author.name',
-      categories: 'tags',
-      description(model) {
-        const excerpt = getExcerpt(get(model, 'html'), {
-          words: 33
-        })
-        return `${excerpt}...`;
-      },
-      url(model, globalConfig) {
-        return `${globalConfig.url}${model.id}/`
-      },
-    };
-  },
 
   model(params) {
     return hash({
---
+++ app/services/head-data.js
@@ -0,0 +1,47 @@
+import HeadData from 'ember-meta/services/head-data';
+import { computed, get } from '@ember/object';
+import { getOwner } from '@ember/application';
+
+import config from '../config/environment';
+
+import { getExcerpt } from '../helpers/excerpt';
+
+export default HeadData.extend({
+  author: computed('routeName', function() {
+    return this.get('currentRouteModel.author.name');
+  }),
+
+  currentRouteModel: computed('routeName', function() {
+    return getOwner(this).lookup(`route:${this.get('routeName')}`).get('currentModel.post');
+  }),
+
+  description: computed('routeName', function() {
+    let currentModel = this.get('currentRouteModel');
+
+    if(currentModel && get(currentModel, 'html')) {
+      const excerpt = getExcerpt(get(currentModel, 'html'), {
+        words: 33
+      })
+      return `${excerpt}...`;
+    }
+
+    return config['ember-meta'].description;
+  }),
+
+  url: computed('routeName', function() {
+    let url = config['ember-meta'].url;
+    const slug = this.get('currentRouteModel.id');
+    if (slug) {
+      url = `${url}${slug}/`;
+    }
+    return url;
+  }),
+
+  categories: computed('routeName', function() {
+    return this.get('currentRouteModel.tags');
+  }),
+
+  date: computed('routeName', function() {
+    return this.get('currentRouteModel.date');
+  }),
+});
