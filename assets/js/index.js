bf9f51e9278d6881d3baf90c1e0452668ca95e87
Dmitriy Schukin <schukindmitriy@gmail.com>
2021-01-28 21:23:16 +0800

    added mobile main menu and draft hashtag toggle, some style fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 import "./lib/gallery-card";
 import "./lib/infinite-scroll";
 import "./lib/jquery.fitvids";
+import "./lib/utils";
 import "./lib/preact";
 import "./lib/smp-search";
 import "./lib/sticky-nav-title";
