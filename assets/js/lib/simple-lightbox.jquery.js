1dcd2b6f1a5b6f866a5b08f83cd39592f00f489d
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2021-09-16 19:14:56 +1200

    Changed lightbox to jQuery version

--- assets/js/lib/simple-lightbox.jquery.js
+++ assets/js/lib/simple-lightbox.jquery.js
@@ -5,6 +5,19 @@
 	Version 2.8.1
 */
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+require('./simple-lightbox');
+
+(function ($, window, document, undefined) {
+  'use strict';
+
+  $.fn.simpleLightbox = function (options) {
+    return this.length ? new SimpleLightbox(this.get(), options) : null;
+  };
+})(jQuery, window, document);
+
+},{"./simple-lightbox":2}],2:[function(require,module,exports){
 (function (global){(function (){
 "use strict";
 
