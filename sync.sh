1c6e47b475e6a59a4bec86f90b2f5c795cb0b57c
Tobias Oetiker <tobi@oetiker.ch>
2022-12-19 14:44:57 +0100

    remove sync

--- sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#ssh ojweb@freddielx 'export PATH=/home/ojweb/.volta/bin:/bin:/usr/bin;cd ojghost/content/themes/oj-ghost-theme && git pull && cd ~/ojghost && ghost restart'
-ssh ojweb@freddielx 'export PATH=/home/ojweb/.volta/bin:/bin:/usr/bin;cd ojghost/content/themes/; rm -rf oj-ghost-theme && git clone https://github.com/olten-jetzt/oj-ghost-theme.git && cd ~/ojghost && ghost restart'
