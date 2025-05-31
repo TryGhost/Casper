c7dc28dfd46e9fa7ec364320098ed1863a6ee837
Brad Wenner <brad@bradwenner.com>
2025-05-31 12:57:59 -0700

    Add flags

--- assets/js/leaflet-custom-icons.js
+++ assets/js/leaflet-custom-icons.js
@@ -2,11 +2,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Define custom icons
     window.customIcons = {
+        // DC colored bright flagred
+        flag: L.icon({
+            iconUrl: '/assets/images/icons/flag.png',
+            iconSize: [56, 60],     // size of the icon
+            iconAnchor: [12, 54],   // point of the icon which will correspond to marker's location
+            popupAnchor: [0, -40]   // point from which the popup should open relative to the iconAnchor
+        }),
         // Red flag icon
-        redflag: L.icon({
-            iconUrl: '/assets/images/redflag.png',
-            iconSize: [30, 40],     // size of the icon
-            iconAnchor: [15, 40],   // point of the icon which will correspond to marker's location
+        flagred: L.icon({
+            iconUrl: '/assets/images/icons/flagred.png',
+            iconSize: [56, 60],     // size of the icon
+            iconAnchor: [12, 54],   // point of the icon which will correspond to marker's location
+            popupAnchor: [0, -40]   // point from which the popup should open relative to the iconAnchor
+        }),
+        // Yellow flag icon
+        flagyellow: L.icon({
+            iconUrl: '/assets/images/icons/flagyellow.png',
+            iconSize: [52, 60],     // size of the icon
+            iconAnchor: [7, 56],   // point of the icon which will correspond to marker's location
             popupAnchor: [0, -40]   // point from which the popup should open relative to the iconAnchor
         })
     };
